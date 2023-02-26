aaa1c2dc9e9c0d2767e57fcc2bdb952e02f117ea
Nicholas Gendron <52512014+nickgendron@users.noreply.github.com>
2023-02-25 18:33:20 -0600

    Delete backend directory

--- backend/src/main/java/com/springbackend/app/rest/User/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.springbackend.app.rest.User;
-
-import com.google.gson.JsonArray;
-import org.springframework.data.repository.CrudRepository;
-
-import java.lang.reflect.Type;
-
-public interface UserRepo extends CrudRepository<User, Integer> {
-}
