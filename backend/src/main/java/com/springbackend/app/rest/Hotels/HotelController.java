d7fcc3a6e39bd2baf3e01bb3fc1b8a61d26e507b
Chloe Heitmeier <46697271+cjh2001@users.noreply.github.com>
2023-03-15 14:05:39 -0500

    Hotel?

---
+++ backend/src/main/java/com/springbackend/app/rest/Hotels/HotelController.java
@@ -0,0 +1,12 @@
+package com.springbackend.app.rest.Hotels;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import java.util.Arrays;
+import java.util.stream.*;
+import java.util.List;
+
+public class HotelController {
+    
+}
